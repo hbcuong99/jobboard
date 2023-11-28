ec56a6e8e7fc6238f44e3203354891010b422bd8
hbcuong <hbcuong@cmcglobal.vn>
2023-11-28 22:09:45 +0700

    Add new project jobboard

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Js custom -->
+    <link rel="stylesheet" href="{{ asset('access/css/custom-bs.css') }}">
+    <link rel="stylesheet" href="{{ asset('access/css/jquery.fancybox.min.css) }}">
+    <link rel="stylesheet" href="{{ asset('access/css/bootstrap-select.min.css) }}">
+    <link rel="stylesheet" href="{{ asset('access/fonts/icomoon/style.css) }}">
+    <link rel="stylesheet" href="{{ asset('access/fonts/line-icons/style.css) }}">
+    <link rel="stylesheet" href="{{ asset('access/css/owl.carousel.min.css) }}">
+    <link rel="stylesheet" href="{{ asset('access/css/animate.min.css) }}">
+    <link rel="stylesheet" href="{{ asset('access/css/quill.snow.css) }}">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="{{ asset('access/css/style.css) }}">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <!-- SCRIPTS -->
+    <script src="{{ asset('access/js/jquery.min.js) }}"></script>
+    <script src="{{ asset('access/js/bootstrap.bundle.min.js) }}"></script>
+    <script src="{{ asset('access/js/isotope.pkgd.min.js) }}"></script>
+    <script src="{{ asset('access/js/stickyfill.min.js) }}"></script>
+    <script src="{{ asset('access/js/jquery.fancybox.min.js) }}"></script>
+    <script src="{{ asset('access/js/jquery.easing.1.3.js) }}"></script>
+    
+    <script src="{{ asset('access/js/jquery.waypoints.min.js) }}"></script>
+    <script src="{{ asset('access/js/jquery.animateNumber.min.js) }}"></script>
+    <script src="{{ asset('access/js/owl.carousel.min.js) }}"></script>
+    <script src="{{ asset('access/js/quill.min.js) }}"></script>
+    
+    
+    <script src="{{ asset('access/js/bootstrap-select.min.js) }}"></script>
+    
+    <script src="{{ asset('access/js/custom.js) }}"></script>
+</body>
+</html>
